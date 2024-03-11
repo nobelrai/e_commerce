026418d84f7295f6da32bf4406082d2d80c3a482
nobelrai <mailtonobelrai@gmail.com>
2024-03-11 18:42:32 +0545

    details

--- home/models.py
+++ home/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-CATEGORY_CHOICES = (
-    ('pants','Pants'),
-    ('shirts', 'Shirts'),
-    ('shoes', 'Shoes'),
-    ('jackets', 'Jackets'),
-)
 
-SIZE_CHOICES = (
-    ('xs', 'XS'),
-    ('s', 'S'),
-    ('l', 'L'),
-    ('xl', 'XL'),
-    ('xll', 'XLL'),
-)
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
 
-COLOR_CHOICES = (
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+class Product(models.Model):
+    COLOR_CHOICES = (
     ('red', 'Red'),
     ('green', 'Green'),
     ('blue', 'Blue'),
@@ -23,12 +18,17 @@ COLOR_CHOICES = (
     ('yellow', 'Yellow'),
     ('pink', 'Pink'),
     ('brown', 'Brown'),
-)
-
-class Product(models.Model):
+    )
+    SIZE_CHOICES = (
+    ('xs', 'XS'),
+    ('s', 'S'),
+    ('l', 'L'),
+    ('xl', 'XL'),
+    ('xll', 'XLL'),
+    )
     name = models.CharField(max_length = 20)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
-    category = models.CharField(max_length = 20, choices = CATEGORY_CHOICES)
     price = models.DecimalField(
     max_digits=10,
     decimal_places=2,
